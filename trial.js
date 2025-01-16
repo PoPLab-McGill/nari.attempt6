21751dd3f309d978166bcb8802525efb26df393d
Psychology of Pragmatics Lab <poplab.scsd@gmail.com>
2025-01-16 12:59:07 -0500

    Update trial.js

--- trial.js
+++ trial.js
@@ -1,4 +1,4 @@
-define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/nari.attempt6@main/extension.js'], function(APIConstructor, stiatExtension){
+define(['pipAPI', 'https://cdn.jsdelivr.net/gh/PoPLab-McGill/nari.attempt7@main/extension.js'], function(APIConstructor, stiatExtension){
     var API = new APIConstructor();
 
     return stiatExtension({
